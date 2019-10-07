41fa4dce2a9fdfbed24fe1877eb33243de78aa2a
TriveniJatla05 <tjatla@student.bridgew.edu>
2019-10-07 15:58:10 -0400

    final changes

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict"; 
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -26,4 +26,4 @@
     offset: 56
   });
 
-})(jQuery); // End of use strict
+})(jQuery); 
